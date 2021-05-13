5e2cff7e6b228b2a8cb2619c3d29c2aa8f935d50
LucasAlt40 <lucas.alt40@gmail.com>
2021-05-13 19:12:08 -0300

    Tentando achar outro meio para o javascript

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
+const opcao = document.querySelectorAll(".opcao");
 const perguntaC = document.querySelectorAll(".correta");
 const perguntaE = document.querySelectorAll(".errada");
+const teste = document.querySelector(".teste");
+
+for(pergunta of perguntaC) {
+  pergunta.onclick = () => {
+    const per = this.document.querySelector(".correta")
+    per.classList.add("acertou")
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const perguntaC = document.querySelectorAll(".correta");
+const perguntaE = document.querySelectorAll(".errada");
 const opcao = document.querySelectorAll(".opcao");
-console.log(opcao)
+const teste = document.querySelector(".teste");
+console.log(opcao);
 var pontos = 0;
 
-perguntaC.forEach((pergunta) => {
-  pergunta.addEventListener("click", () => {
-    pergunta.classList.add("acertou");
-    this.opcao.classList.add("bloqueado");
-    pontos++;
-    console.log(pontos);
+for (op of opcao) {
+  perguntaC.forEach((pergunta) => {
+    pergunta.addEventListener("click", () => {
+      pergunta.classList.add("acertou");
+      const test = this.document.querySelector(".teste");
+      test.classList.add("bloqueado");
+      pontos++;
+      console.log(pontos);
+    });
   });
-});
 
-perguntaE.forEach((pergunta) => {
-  pergunta.addEventListener("click", () => {
-    pergunta.classList.add("errou");
-    this.opcao.classList.add("bloqueado");
+  perguntaE.forEach((pergunta) => {
+    pergunta.addEventListener("click", () => {
+      pergunta.classList.add("errou");
+      const test = this.document.querySelector(".teste");
+      test.classList.add("bloqueado");
+    });
   });
-});
+} */
